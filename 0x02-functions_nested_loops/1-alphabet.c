4ea92eead5d40a71e7bd26fc2ebb7fc740e9ba69
Baczee <banndlovu94@gmail.com>
2023-02-21 06:03:38 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+
+	}
+	_putchar('\n');
+}

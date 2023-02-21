995104ddd9d45f98744d5b83aba6437351b78c3d
Baczee <banndlovu94@gmail.com>
2023-02-21 07:11:03 -0800

    main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,11 @@
 
 /**
  *print_alphabet - Entry point
- * Description: a function that prints the alphabet,
+ *Description: a function that prints the alphabet,
  *in lowecase followed by a new line
  *Return: void
  */
+
 void print_alphabet(void)
 {
 	char letter = 'a';
